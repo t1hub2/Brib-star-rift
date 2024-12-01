7737ff98cd60f4f8d874ee55aadb6de8d5126458
t1hub2 <solavolodimir@gmail.com>
2024-12-01 10:22:46 +0100

    Create blank.lua

---
+++ .github/workflows/blank.lua
@@ -0,0 +1,7 @@
+workspace.CurrentRooms["0"].RiftSpawn.Rift.StarCenter.StarRiftPrompt.Enabled = true
+                    workspace.CurrentRooms["0"].RiftSpawn.Rift.StarCenter.Rift.Transparency = 0
+                end
+            }
+        }
+    }
+}
